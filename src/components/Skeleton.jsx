68aa43def58dd41006b05f318a619bb50da69744
guptavikramdev <myselfvikramgupta@gmail.com>
2023-08-08 23:11:31 +0530

    warning issue fixed

--- src/components/Skeleton.jsx
+++ src/components/Skeleton.jsx
@@ -2,7 +2,7 @@ import React from "react";
 const Skeleton = ({ size, w, h, rounded = "" }) => {
   return (
     <>
-      <h3 className="w-96 h-8 my-5 bg-slate-100 dark:bg-slate-800 rounded-3xl"></h3>
+      <div className="w-96 h-8 my-5 bg-slate-100 dark:bg-slate-800 rounded-3xl"></div>
       <div className="flex w-full gap-4">
         {Array(size)
           .fill()
--- src/components/common/Image.jsx
+++ src/components/common/Image.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import {CLOUDINARY_URL} from "../../utils/constant"
-const Image = ({className,imgUrl }) => {
-
+import { CLOUDINARY_URL } from "../../utils/constant";
+const Image = ({ className, imgUrl }) => {
   return (
     <img
       src={`${CLOUDINARY_URL}/${imgUrl}`}
       className={className}
       loading="lazy"
+      alt=""
     />
   );
 };
